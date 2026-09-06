823cf46d0bafba14bccf1a96c2f91cb1db89b233
Nate Tillman <tillmanlafa26@gmail.com>
2026-09-06 13:14:58 -0400

    converted to react

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+﻿import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "../CSS/styles.css";
+import "../CSS/calendar.css";
+
+createRoot(document.getElementById("root")).render(<StrictMode><App /></StrictMode>);
